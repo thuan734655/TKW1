75eb7b34fffd69728a82eaa0ae8fe7592c9a8bcb
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-06-21 17:12:23 +0700

    fix deploy #1

--- docs/quiiz.3ec3c142.js
+++
@@ -1,2 +0,0 @@
-var e=document.querySelectorAll(".right-content--iconArrow"),r=document.querySelectorAll(".right-content--list");Array.from(e).forEach((e,t)=>{var l=r[t],o=!1;e.addEventListener("click",()=>{o?(l.style.display="none",o=!1):(l.style.display="block",o=!0)})});
-//# sourceMappingURL=quiiz.3ec3c142.js.map
--- public/login.422205ce.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#sign-in-button"),t=document.querySelector("#sign-up-button"),n=document.querySelector(".container");t.addEventListener("click",()=>{n.classList.add("sign-up-mode")}),e.addEventListener("click",()=>{n.classList.remove("sign-up-mode")});
-//# sourceMappingURL=login.422205ce.js.map
