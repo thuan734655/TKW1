715657009fcd3833506b95cd59c869b29c4ea804
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-06-21 12:08:09 +0700

    fix deploy #11

--- public/quiiz.d66ecb2e.js
+++
@@ -1,2 +0,0 @@
-let e=0;const t=document.querySelector("header");window.addEventListener("scroll",()=>{let n=window.pageYOffset||document.documentElement.scrollTop;n>e?t.classList.add("hide"):t.classList.remove("hide"),e=n<=0?0:n}),document.querySelector(".header__content--menu").addEventListener("click",()=>{let e=document.querySelector(".header__content--pane-menu");e.style.left="0px"===e.style.left?"-350px":"0px"});
-//# sourceMappingURL=quiiz.d66ecb2e.js.map
