89fda50c4fe535cc4b4357b6d1466768dc383d23
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-06-21 16:53:01 +0700

    upload #3

--- src/javascript/header.js
+++ src/javascript/header.js
@@ -1,33 +1,22 @@
-var lastScrollTop = 0;
-let header = document.querySelector('header');
-let menuButton = document.querySelector('.header__content--menu');
-let paneMenu = document.querySelector('.header__content--pane-menu');
+let lastScrollTop = 0;
+const header = document.querySelector('header');
 
-// Chờ tài liệu DOM tải hoàn toàn trước khi tương tác với nó
-document.addEventListener('DOMContentLoaded', () => {
-  header = document.querySelector('header');
-  menuButton = document.querySelector('.header__content--menu');
-  paneMenu = document.querySelector('.header__content--pane-menu');
+window.addEventListener('scroll', () => {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-  if (header && menuButton && paneMenu) {
-    window.addEventListener('scroll', () => {
-      let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-      if (scrollTop > lastScrollTop) {
-        // Cuộn xuống, ẩn header
-        header.classList.add('hide');
-      } else {
-        // Cuộn lên, hiện header
-        header.classList.remove('hide');
-      }
-
-      lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Tránh vị trí cuộn âm
-    });
-
-    menuButton.addEventListener('click', () => {
-      paneMenu.style.left = paneMenu.style.left === '0px' ? '-350px' : '0px';
-    });
+  if (scrollTop > lastScrollTop) {
+    // Cuộn xuống, ẩn header
+    header.classList.add('hide');
   } else {
-    console.error('Một hoặc nhiều phần tử cần thiết không tìm thấy.');
+    // Cuộn lên, hiện header
+    header.classList.remove('hide');
   }
+
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Tránh vị trí cuộn âm
+});
+
+// Hiển thị menu khi click
+document.querySelector('.header__content--menu').addEventListener('click', () => {
+  const paneMenu = document.querySelector('.header__content--pane-menu');
+  paneMenu.style.left = paneMenu.style.left === '0px' ? '-350px' : '0px';
 });
